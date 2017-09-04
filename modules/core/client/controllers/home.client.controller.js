fd3004d3899a92e89de83b933c04f03a1dc1e2b4
KasunGunarathna <kasunpradeepa.gunarathna@gmail.com>
2017-09-04 19:20:10 +0530

    ngRepeat

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -1,8 +1,34 @@
 'use strict';
 
 angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-  function ($scope, Authentication) {
-    // This provides Authentication context.
-    $scope.authentication = Authentication;
-  }
+    function($scope, Authentication) {
+        // This provides Authentication context.
+        $scope.authentication = Authentication;
+
+        $scope.alerts = [{
+                color: 'btn-success',
+                total: '20,453'
+            },
+            {
+                color: 'btn-success',
+                total: '20,453'
+            },
+            {
+                color: 'btn-success',
+                total: '20,453'
+            },
+            {
+                color: 'btn-success',
+                total: '20,453'
+            },
+            {
+                color: 'btn-success',
+                total: '20,453'
+            }
+
+        ];
+
+
+
+    }
 ]);
